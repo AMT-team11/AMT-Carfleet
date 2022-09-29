c3752efdca249de2ab8317b1b01c1ee11e654d25
NunezTania <tnunez@bluewin.ch>
2022-09-29 19:03:53 +0200

    Adding maven configuration

--- src/main/java/ParserJSON.java
+++ src/main/java/ParserJSON.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class ParserJSON {
 
     public void parse(File file) throws EmptyJSONException{
-        throw new UnsupportedOperationException();
+        throw new EmptyJSONException();
     }
 
     class EmptyJSONException extends Exception {
