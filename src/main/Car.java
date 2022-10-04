528bc379e1a6d6b9543993c579d86102e63191bb
magskwa <magali.egger@heig-vd.ch>
2022-10-04 17:09:01 +0200

    add constructors getter and setter

--- src/main/Car.java
+++ src/main/Car.java
@@ -6,4 +6,34 @@ public class Car {
     private String id;
     private String name;
     private ArrayList<Values> values;
+
+    public Car(String id, String name, ArrayList<Values> values) {
+        this.id = id;
+        this.name = name;
+        this.values = values;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Values> getValues() {
+        return values;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValues(ArrayList<Values> values) {
+        this.values = values;
+    }
 }
--- src/main/Driver.java
+++ src/main/Driver.java
@@ -7,4 +7,33 @@ public class Driver {
     private String name;
     private ArrayList<Car> cars;
 
+    public Driver(String id, String name, ArrayList<Car> cars) {
+        this.id = id;
+        this.name = name;
+        this.cars = cars;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCars(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
 }
